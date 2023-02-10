9dd25e730a8da8c349538adb8c7b0247f3653a5d
krishna <brkkrishna9@gmail.com>
2023-02-11 01:07:05 +0530

    new changes

---
+++ env-dev/state.tfvars
@@ -0,0 +1,3 @@
+bucket = "terraform-brk"
+key    = "roboshop-infra/dev/terraform.tfstate"
+region = "us-east-1"
---
+++ main.tf
@@ -0,0 +1,7 @@
+module "network" {
+  source = "github.com/raghudevopsb70/tf-module-vpc"
+  env    = var.env
+
+  for_each   = var.vpc
+  cidr_block = each.value.cidr_block
+}
