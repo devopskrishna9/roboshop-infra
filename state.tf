02c4e6f103252ca6a6514099fb9249b25e2d9ab7
krishna <brkkrishna9@gmail.com>
2023-02-12 21:20:57 +0530

    new changes

--- state.tf
+++ state.tf
@@ -1,6 +1,7 @@
-
-backend "s3" {
-  bucket = "terraform-brk"
-  key    = "roboshop-infra/dev/terraform.tfstate"
-  region = "us-east-1"
+terraform {
+  backend "s3" {
+    bucket = "terraform-brk"
+    key    = "roboshop-infra/dev/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
