8866425112543fb38df7b9ee7a3886bddc63df7f
krishna <brkkrishna9@gmail.com>
2023-02-12 23:48:51 +0530

    new changes

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
-resource "null_resource" "test" {
-  provisioner "local-exec" {
-    command = "echo ${var.env}"
-  }
+#resource "null_resource" "test" {
+#  provisioner "local-exec" {
+#    command = "echo ${var.env}"
+#  }
+#}
+
+module "network" {
+  source = "github.com/devopskrishna9/tf-module-vpc"
+  for_each = var.vpc
+  cidr_block = each.value.cidr_block
+
 }
-
-
