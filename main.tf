02893d9fb739ef844662b8303d8ba9bf1ea86446
krishna <brkkrishna9@gmail.com>
2023-03-22 21:48:03 +0530

    new changes made

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ module "apps" {
   source = "github.com/devopskrishna9/tf-module-app"
   env = var.env
 
-  //depends_on = [module.docdb, module.rds, module.rabbitmq, module.alb, module.rds, module.elasticache]
+  depends_on = [module.docdb, module.rds, module.rabbitmq, module.alb, module.rds, module.elasticache]
 
   for_each     = var.apps
   subnet_ids       = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), each.value.subnets_type, null), each.value.subnets_name, null), "subnet_ids", null)
